33546d5cc7544c0749dff0078c2a6756e3deeb1b
Andrew <arhorn@smcm.edu>
2018-02-21 11:39:35 +0700

    created HtmlListingGenerator, moved ReactQuill into it

--- src/components/DiffChecker/DiffChecker.js
+++ src/components/DiffChecker/DiffChecker.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import ReactQuill from 'react-quill';
 
 class DiffChecker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { text: '' } // You can also pass a Quill Delta here
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(value) {
-    this.setState({ text: value })
-  }
-
+  
+  
   render() {
     return (
-      <ReactQuill value={this.state.text}
-        onChange={this.handleChange} />
-    )
+      <div>
+        <p>Hello from DiffChecker!</p>
+      </div>
+    );
   }
-}
+};
 
 export default DiffChecker;
