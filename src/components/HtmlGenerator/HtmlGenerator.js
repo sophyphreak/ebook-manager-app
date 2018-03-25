d7c00079159cf137802571aed6caf35372ccb5e7
Andrew <arhorn@smcm.edu>
2018-03-25 22:01:27 +0700

    Fixed HtmlGenerator. Now smoothly switches from desktop to mobile view

--- src/components/HtmlGenerator/HtmlGenerator.js
+++ src/components/HtmlGenerator/HtmlGenerator.js
@@ -9,29 +9,14 @@ export default class HtmlGenerator extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-      text: '',
-      width: window.innerWidth
+      text: ''
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleWindowSizeChange = this.handleChange.bind(this);
   }
-
-  componentWillMount() {
-    window.addEventListener('resize', this.handleWindowSizeChange);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleWindowSizeChange);
-  }
-  handleWindowSizeChange() {
-    this.setState({ width: window.innerWidth });
-  };
   handleChange(value) {
     this.setState({ text: value })
   }
   render() {
-    const { width } = this.state;
-    const isMobile = width <= 500;
-
     const modules = {
       toolbar: [
         [{ 'header': [1, 2, 3, false] }],
@@ -43,42 +28,22 @@ export default class HtmlGenerator extends React.Component {
         ['clean']
       ],
     };
-
-    if (isMobile) {
-      return (
-        <div>
-          <Row>
-            <ReactQuill
-              value={this.state.text}
-              onChange={this.handleChange}
-              modules={modules}
-            />
-          </Row>
-          <Row>
-            <textarea
-              value={this.state.text}
-            />
-          </Row>
-        </div>
-      );
-    } else {
-      return(
-        <Row>
-          <Col xs="12" sm="6">
-            <ReactQuill
-              value={this.state.text}
-              onChange={this.handleChange}
-              modules={modules}
-            />
-          </Col>
-          <Col xs="12" sm="6">
-            <textarea
-              className="textarea ql-editor"
-              value={this.state.text}
-            />
-          </Col>
-        </Row>
-      );
-    }
+    return (
+      <Row>
+        <Col xs="12" sm="6">
+          <ReactQuill
+            value={this.state.text}
+            onChange={this.handleChange}
+            modules={modules}
+          />
+        </Col>
+        <Col xs="12" sm="6">
+          <textarea
+            className="textarea ql-editor"
+            value={this.state.text}
+          />
+        </Col>
+      </Row>
+    );
   }
-};
+}
