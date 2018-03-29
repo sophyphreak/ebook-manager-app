f08f64d610de746af1af7aa9834e0415797aec12
Andrew <arhorn@smcm.edu>
2018-03-29 15:19:04 +0700

    Fixed testing suite for recent changes

--- src/tests/containers/AlertsSubmitter/alertsPostToServer/alertsPostToServer.test.js
+++ src/tests/containers/AlertsSubmitter/alertsPostToServer/alertsPostToServer.test.js
@@ -1,4 +1,4 @@
-import alertsPostToServer from '../../../../containers/AlertsSubmitter/alertsPostToServer/alertsPostToServer';
+import alertToNodemailer from '../../../../containers/AlertsSubmitter/alertToNodemailer/alertToNodemailer';
 
 test('1 should equal 1', () => {
   expect(1).toBe(1);
