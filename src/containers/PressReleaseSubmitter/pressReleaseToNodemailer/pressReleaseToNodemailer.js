a330fc08451c05375c91825b4a3a699bb88c476e
Andrew <arhorn@smcm.edu>
2018-03-30 13:15:54 +0700

    Updated test snapshots and PR-to-nodemailer test files

---
+++ src/containers/PressReleaseSubmitter/pressReleaseToNodemailer/pressReleaseToNodemailer.js
@@ -0,0 +1,42 @@
+import getFilteredBodyAndOrder from '../../submitterUtils/getFilteredBodyAndOrder';
+import postToNodemailer from '../../submitterUtils/postToNodemailer';
+
+const pressReleaseToNodemailer = ({
+  amazonURL,
+  fictionOrNonFiction,
+  genre,
+  subGenre,
+  email,
+  price,
+  promoType,
+  startDate,
+  endDate,
+  website,
+  keywords,
+  releaseText
+}) => {
+  const submissionType = 'Press Release';
+  let rawBody = {
+    'Amazon URL': amazonURL,
+    'Fiction or Nonfiction?': fictionOrNonFiction,
+    'Genre': genre,
+    'Sub-genre': subGenre,
+    'Email': email,
+    'Regular Price': price,
+    'Promo Type': promoType,
+    'Start Date': startDate.format("MMMM Do YYYY"),
+    'End Date': endDate.format("MMMM Do YYYY"),
+    'Website': website,
+    'Keywords': keywords,
+    'Press Release': releaseText
+  };
+  const {
+    bodyObject,
+    rowLabelOrder
+  } = getFilteredBodyAndOrder(rawBody);
+  const userEmail = email;
+
+  postToNodemailer(submissionType, bodyObject, rowLabelOrder, userEmail);
+}
+
+export default pressReleaseToNodemailer;
--- src/tests/containers/PressReleaseSubmitter/pressReleasePostToServer/pressReleasePostToServer.test.js
+++
@@ -1,5 +0,0 @@
-import pressReleasePostToServer from '../../../../containers/PressReleaseSubmitter/pressReleasePostToServer/pressRelasePostToServer';
-
-test('1 should equal 1', () => {
-  expect(1).toBe(1);
-});
