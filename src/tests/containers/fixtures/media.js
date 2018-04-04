7a84b3e2a833fb62bdeeb353e3dbd032edfd3faf
Andrew <arhorn@smcm.edu>
2018-04-04 16:49:09 +0700

    created media fixtures for testing

---
+++ src/tests/containers/fixtures/media.js
@@ -0,0 +1,36 @@
+import moment from 'moment';
+
+export default {
+  min: {
+    amazonUrl: 'https://www.amazon.com/Chuang-Tzu-Writings-Burton-Watson/dp/0231105959',
+    fictionOrNonFiction: 'Non-fiction *',
+    genre: '',
+    subGenre: '',
+    email: 'example@example.email',
+    regPrice: '111',
+    salePrice: '',
+    promoType: '99 cents',
+    startDate: null,
+    endDate: null,
+    keywords: '',
+    tweet: '',
+    facebook: '',
+    notes: ''
+  },
+  max: {
+    amazonUrl: 'https://www.amazon.com/Chuang-Tzu-Writings-Burton-Watson/dp/0231105959',
+    fictionOrNonFiction: 'Fiction',
+    genre: 'Romance',
+    subGenre: 'Kink Life',
+    email: 'example@example.email',
+    regPrice: '111',
+    salePrice: '99',
+    promoType: '99 cents',
+    startDate: moment("20501228", "YYYYMMDD"),
+    endDate: moment("20501231", "YYYYMMDD"),
+    keywords: 'wise, fox, app',
+    tweet: 'This is a short tweet',
+    facebook: 'This is a short facebook post',
+    notes: 'This is a short note.'
+  }
+}
