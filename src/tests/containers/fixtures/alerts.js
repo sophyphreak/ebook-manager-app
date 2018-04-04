b1333d77b01933dfa2a2b0a714c7274033ee79a5
Andrew <arhorn@smcm.edu>
2018-04-04 16:13:48 +0700

    Refactored alert fixtures

--- src/tests/containers/fixtures/alerts.js
+++ src/tests/containers/fixtures/alerts.js
@@ -1,6 +1,26 @@
 import moment from 'moment';
 
 export default {
+  min: {
+    amazonUrl: '',
+    alertMe: {
+      onTheDate: {
+        text: 'On the Date',
+        isActive: false
+      },
+      oneWeekBefore: {
+        text: 'One week before',
+        isActive: true
+      },
+      twoWeeksBefore: {
+        text: 'Two weeks before',
+        isActive: false
+      }
+    },
+    notes: 'Min notes.',
+    date: moment("20501228", "YYYYMMDD"),
+    email: 'example@example.email'
+  },
   max: {
     amazonUrl: 'https://www.amazon.com/Chuang-Tzu-Writings-Burton-Watson/dp/0231105959',
     alertMe: {
@@ -21,25 +41,5 @@ export default {
     notes: 'These are some notes.',
     email: 'example@example.email',
     email2: 'secondexample@example.email'
-  },
-  min: {
-    amazonUrl: '',
-    alertMe: {
-      onTheDate: {
-        text: 'On the Date',
-        isActive: false
-      },
-      oneWeekBefore: {
-        text: 'One week before',
-        isActive: true
-      },
-      twoWeeksBefore: {
-        text: 'Two weeks before',
-        isActive: false
-      }
-    },
-    notes: 'Min notes.',
-    date: moment("20501228", "YYYYMMDD"),
-    email: 'example@example.email'    
   }
 }
