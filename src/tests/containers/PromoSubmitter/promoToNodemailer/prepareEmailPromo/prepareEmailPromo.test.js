fd98412c5f7f2f26d628d030ad6f2960a8dc0341
Andrew <arhorn@smcm.edu>
2018-04-04 16:36:17 +0700

    Added testing for prepareEmailPromo

--- src/tests/containers/PromoSubmitter/promoToNodemailer/prepareEmailPromo/prepareEmailPromo.test.js
+++ src/tests/containers/PromoSubmitter/promoToNodemailer/prepareEmailPromo/prepareEmailPromo.test.js
@@ -1,12 +1,98 @@
-// import prepareEmailPromo from '../../../../../containers/PromoSubmitter/promoToNodemailer/prepareEmailPromo/prepareEmailPromo';
-// import alert from '../../../fixtures/alert';
+import prepareEmailPromo from '../../../../../containers/PromoSubmitter/promoToNodemailer/prepareEmailPromo/prepareEmailPromo';
+import promos from '../../../fixtures/promos';
 
-// let state;
+test('should convert min promo into correct object', () => {
+  const prepared = prepareEmailPromo(promos.min);
+  const {
+    submissionType,
+    emailBody,
+    rowOrder,
+    userEmail
+  } = prepared;
+  const {
+    amazonUrl,
+    fictionOrNonFiction,
+    email,
+    price,
+    promoType,
+    startDate,
+    endDate,
+    description,
+    authorBio
+  } = promos.min;
+  expect(submissionType).toBe('Book Promotion');
+  expect(emailBody).toEqual({
+    'Amazon URL': amazonUrl,
+    'Fiction or Nonfiction?': fictionOrNonFiction,
+    'Email': email,
+    'Regular Price': price,
+    'Promo Type': promoType,
+    'Start Date': startDate.format("MMMM Do YYYY"),
+    'End Date': endDate.format("MMMM Do YYYY"),
+    'Description': description,
+    'Author Biography': authorBio
+  });
+  expect(rowOrder).toEqual([
+    'Amazon URL',
+    'Fiction or Nonfiction?',
+    'Email',
+    'Regular Price',
+    'Promo Type',
+    'Start Date',
+    'End Date',
+    'Description',
+    'Author Biography'
+  ]);
+  expect(userEmail).toBe(email);
+});
 
-// beforeEach(() => {
-//   state = alert;
-// });
-
-// test('should convert into correct object', () => {
-
-// })
+test('should convert max promo into correct object', () => {
+  const prepared = prepareEmailPromo(promos.max);
+  const {
+    submissionType,
+    emailBody,
+    rowOrder,
+    userEmail
+  } = prepared;
+  const {
+    amazonUrl,
+    fictionOrNonFiction,
+    genre,
+    subGenre,
+    email,
+    price,
+    promoType,
+    startDate,
+    endDate,
+    description,
+    authorBio
+  } = promos.max;
+  expect(submissionType).toBe('Book Promotion');
+  expect(emailBody).toEqual({
+    'Amazon URL': amazonUrl,
+    'Fiction or Nonfiction?': fictionOrNonFiction,
+    'Genre': genre,
+    'Sub-genre': subGenre,
+    'Email': email,
+    'Regular Price': price,
+    'Promo Type': promoType,
+    'Start Date': startDate.format("MMMM Do YYYY"),
+    'End Date': endDate.format("MMMM Do YYYY"),
+    'Description': description,
+    'Author Biography': authorBio
+  });
+  expect(rowOrder).toEqual([
+    'Amazon URL',
+    'Fiction or Nonfiction?',
+    'Genre',
+    'Sub-genre',
+    'Email',
+    'Regular Price',
+    'Promo Type',
+    'Start Date',
+    'End Date',
+    'Description',
+    'Author Biography'
+  ]);
+  expect(userEmail).toBe('example@example.email');
+});
---
+++ src/tests/containers/fixtures/promos.js
@@ -0,0 +1,30 @@
+import moment from 'moment';
+
+export default {
+  min: {
+    amazonUrl: 'https://www.amazon.com/Chuang-Tzu-Writings-Burton-Watson/dp/0231105959',
+    fictionOrNonFiction: 'Non-fiction *',
+    genre: '',
+    subGenre: '',
+    email: 'example@example.email',
+    price: '111',
+    promoType: '99 cents',
+    startDate: moment("20501228", "YYYYMMDD"),
+    endDate: moment("20501231", "YYYYMMDD"),
+    description: 'Sample description.',
+    authorBio: 'Sample author biography.'
+  },
+  max: {
+    amazonUrl: 'https://www.amazon.com/Chuang-Tzu-Writings-Burton-Watson/dp/0231105959',
+    fictionOrNonFiction: 'Fiction',
+    genre: 'Romance',
+    subGenre: 'Kink Life',
+    email: 'example@example.email',
+    price: '111',
+    promoType: '99 cents',
+    startDate: moment("20501228", "YYYYMMDD"),
+    endDate: moment("20501231", "YYYYMMDD"),
+    description: 'Sample description.',
+    authorBio: 'Sample author biography.'
+  }
+}
