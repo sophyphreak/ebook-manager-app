f1df1c49175e3a8b075e969eacef9cf633bbad8f
Andrew <arhorn@smcm.edu>
2018-04-06 23:34:34 +0700

    Reformatted Dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'reactstrap';
+
 import nothingToSeeGif from '../../../public/img/nothing-to-see-here.gif';
 
 class Dashboard extends Component {
 
   render() {
     return (
-      <div className="animated fadeIn">
-        <h4>Your Dashboard</h4>
-        <img src={nothingToSeeGif} />
-      </div>
+      <Row className="animated fadeIn">
+        <Col xs="12" sm={{ size: 6, offset: 3 }}>
+          <h4>Your Dashboard</h4>
+          <img src={nothingToSeeGif} />
+        </Col>
+      </Row>
     )
   }
 }
