bfe915d28fc9e9c7157c85748889ee41abf74817
Andrew <arhorn@smcm.edu>
2018-04-07 14:27:20 +0700

    Whole success page now fades in

--- src/components/SuccessPage/SuccessPage.js
+++ src/components/SuccessPage/SuccessPage.js
@@ -2,8 +2,8 @@ import React from 'react';
 import greatSuccessGif from '../../../public/img/great-success.gif'
 
 const SuccessPage = ({ feature }) => (
-  <div>
-    <p className="animated fadeIn">
+  <div className="animated fadeIn">
+    <p>
       Your {feature} has been submitted. Please wait for a
       confirmation email.
     </p>
