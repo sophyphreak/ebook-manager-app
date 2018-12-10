cde156fd3c58df6e109eaa869baaa3da179c86e3
Andrew <arhorn@smcm.edu>
2018-12-11 04:24:57 +0800

    removed useless comments

--- server/server.js
+++ server/server.js
@@ -11,10 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
   app.use(express.static(path.join(__dirname, '..', 'build')));
 
-  // Handle React routing, return all requests to React app
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
   });
